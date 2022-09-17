3edbed60d9a07d644bdfa45d87581a5b7ef885cd
Abduqodirov Muhammadxon <abduqodirovmuhammadhon@outlook.com>
2022-09-17 04:42:27 -0700

    initial commit

--- target/debug/build/winapi-8012ffd823a1e43e/build_script_build-8012ffd823a1e43e.d
+++
@@ -1,5 +0,0 @@
-c:\Users\abduq\Documents\GitHub\dgews\target\debug\build\winapi-8012ffd823a1e43e\build_script_build-8012ffd823a1e43e.exe: C:\Users\abduq\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.9\build.rs
-
-c:\Users\abduq\Documents\GitHub\dgews\target\debug\build\winapi-8012ffd823a1e43e\build_script_build-8012ffd823a1e43e.d: C:\Users\abduq\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.9\build.rs
-
-C:\Users\abduq\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.9\build.rs:
